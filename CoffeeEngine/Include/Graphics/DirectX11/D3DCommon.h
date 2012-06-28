ec8aeff0baeba766f0caefbe6e965ea94c6ec568
Ken Anderson <CaffeinatedRat@gmail.com>
2012-06-28 19:51:39 -0400

    Moved the DirectX headers here and added additional DX structures.

--- CoffeeEngine/Include/Graphics/DirectX11/D3DCommon.h
+++ CoffeeEngine/Include/Graphics/DirectX11/D3DCommon.h
@@ -2,11 +2,17 @@
 // Description: Specific global variables, functions, and definitions for DirectX.
 //
 // Copyright (c) 2012 Ken Anderson <caffeinatedrat@gmail.com>
+// http://www.caffeinatedrat.com
 //--------------------------------------------------------------------------------------
 
 #pragma once
 #ifndef _D3D_COMMON_H_
 #define _D3D_COMMON_H_
+
+#include <dxgi.h>
+#include <d3dcommon.h>
+#include <d3d11.h>
+#include <d3dx10math.h>
 
 #define SAFE_RELEASE(p) if (p != NULL) { p->Release(); p = NULL; }
 
@@ -18,6 +24,21 @@ namespace CoffeeEngine
 		{
 			extern const char* VERTEX_SHADER_VERSION;
 			extern const char* PIXEL_SHADER_VERSION;
+
+			typedef struct SimpleMatrixBufferType
+			{
+				D3DXMATRIX world;
+				D3DXMATRIX view;
+				D3DXMATRIX projection;
+			} SimpleMatrixBufferType;
+
+			typedef struct SimpleVertexType
+			{
+				D3DXVECTOR3 position;
+				D3DXVECTOR4 color;
+				D3DXVECTOR2 texture;
+				//D3DXVECTOR3 normal;
+			} SimpleVertexType;
 		}
 	}
 }
