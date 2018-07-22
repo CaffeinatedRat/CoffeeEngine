e82bbe5a39b871f9acb7a3b8dac80c6eb283d501
CaffeinatedRat <caffeinatedrat@gmail.com>
2018-07-21 22:52:58 -0400

    NoExcept added to the Vector classes.

--- CoffeeEngine/Src/Math/Vectors.hpp
+++ CoffeeEngine/Src/Math/Vectors.hpp
@@ -16,32 +16,32 @@ namespace CoffeeEngine
 		struct Vector3
 		{
 			float _x, _y, _z;
-			Vector3() : _x(0.0f), _y(0.0f), _z(0.0f) {};
-			Vector3(float x, float y, float z) : _x(x), _y(y), _z(z) {};
-			Vector3(const Vector3& vector) : _x(vector._x), _y(vector._y), _z(vector._z) {};
-			Vector3(Vector3&& vector) : _x(vector._x), _y(vector._y), _z(vector._z)
+			Vector3() noexcept : _x(0.0f), _y(0.0f), _z(0.0f) {};
+			Vector3(float x, float y, float z) noexcept : _x(x), _y(y), _z(z) {};
+			Vector3(const Vector3& vector) noexcept : _x(vector._x), _y(vector._y), _z(vector._z) {};
+			Vector3(Vector3&& vector) noexcept : _x(vector._x), _y(vector._y), _z(vector._z)
 			{
 				vector._x = vector._y = vector._z = 0.0f;
 			};
 
-			Vector3& operator=(Vector3&& vector) = default;
-			Vector3& operator=(const Vector3& vector) = default;
+			Vector3& operator=(Vector3&& vector) noexcept = default;
+			Vector3& operator=(const Vector3& vector) noexcept = default;
 		};
 
 		struct Vector4
 		{
 			float _x, _y, _z, _w;
-			Vector4() : _x(0.0f), _y(0.0f), _z(0.0f), _w(1.0f) {};
-			Vector4(float x, float y, float z, float w) : _x(x), _y(y), _z(z), _w(w) {};
-			Vector4(const Vector4& vector) : _x(vector._x), _y(vector._y), _z(vector._z), _w(vector._w) {};
-			Vector4(Vector4&& vector) : _x(vector._x), _y(vector._y), _z(vector._z), _w(vector._w)
+			Vector4() noexcept : _x(0.0f), _y(0.0f), _z(0.0f), _w(1.0f) {};
+			Vector4(float x, float y, float z, float w) noexcept : _x(x), _y(y), _z(z), _w(w) {};
+			Vector4(const Vector4& vector) noexcept : _x(vector._x), _y(vector._y), _z(vector._z), _w(vector._w) {};
+			Vector4(Vector4&& vector) noexcept : _x(vector._x), _y(vector._y), _z(vector._z), _w(vector._w)
 			{
 				vector._x = vector._y = vector._z = 0.0f;
 				vector._w = 1.0f;
 			};
 
-			Vector4& operator=(Vector4&& vector) = default;
-			Vector4& operator=(const Vector4& vector) = default;
+			Vector4& operator=(Vector4&& vector) noexcept = default;
+			Vector4& operator=(const Vector4& vector) noexcept = default;
 		};
 	}
 }
